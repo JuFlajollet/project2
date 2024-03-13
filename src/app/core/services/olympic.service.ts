b975f4f1c4641a0811266b48e0867cd1704cfca8
JuFlajollet <juflajollet@gmail.com>
2024-03-13 14:30:04 +0100

    Define types for olympic observable

--- src/app/core/services/olympic.service.ts
+++ src/app/core/services/olympic.service.ts
@@ -1,31 +1,32 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { Observable, ReplaySubject } from 'rxjs';
 import { catchError, tap } from 'rxjs/operators';
+import { OlympicCountry } from '../models/Olympic';
 
 @Injectable({
   providedIn: 'root',
 })
 export class OlympicService {
   private olympicUrl = './assets/mock/olympic.json';
-  private olympics$ = new BehaviorSubject<any>(undefined);
+  private olympics$ = new ReplaySubject<OlympicCountry[]>(); // TODO: Check if we need a BehaviourSubject or not (because I don't see which value to initialize with)
 
   constructor(private http: HttpClient) {}
 
-  loadInitialData() {
-    return this.http.get<any>(this.olympicUrl).pipe(
+  loadInitialData(): Observable<OlympicCountry[]> {
+    return this.http.get<OlympicCountry[]>(this.olympicUrl).pipe(
       tap((value) => this.olympics$.next(value)),
       catchError((error, caught) => {
         // TODO: improve error handling
         console.error(error);
         // can be useful to end loading state and let the user know something went wrong
-        this.olympics$.next(null);
+        //this.olympics$.next(null);
         return caught;
       })
     );
   }
 
-  getOlympics() {
+  getOlympics(): Observable<OlympicCountry[]> {
     return this.olympics$.asObservable();
   }
 }
