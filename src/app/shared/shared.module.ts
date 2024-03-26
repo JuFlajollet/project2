18a333455159788c487690993e794638c233ccfe
JuFlajollet <juflajollet@gmail.com>
2024-03-26 09:56:43 +0100

    Add loading spinner during data loading and update readme

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,21 @@ import { NgModule } from "@angular/core";
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { MedalsByCountryPieChartComponent } from "./components/medals-by-country-pie-chart/medals-by-country-pie-chart.component";
 import { MedalsByDatesLineChartComponent } from "./components/medals-by-dates-line-chart/medals-by-dates-line-chart.component";
-import { BrowserModule } from "@angular/platform-browser";
+import { LoadingSpinnerComponent } from "./components/loading-spinner/loading-spinner.component";
 
 @NgModule({
     declarations: [
         MedalsByCountryPieChartComponent,
-        MedalsByDatesLineChartComponent
+        MedalsByDatesLineChartComponent,
+        LoadingSpinnerComponent
     ],
     imports: [
         NgxChartsModule
     ],
     exports: [
         MedalsByCountryPieChartComponent,
-        MedalsByDatesLineChartComponent
+        MedalsByDatesLineChartComponent,
+        LoadingSpinnerComponent
     ]
 })
 export class SharedModule {
